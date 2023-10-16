ecd900c453804e8025d249c35aa721514eb294d0
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-16 16:02:04 +0200

    Task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- This program will print the alphabet
+ * with the putchar function in lowercase then in UPPERCASE
+ * Return: 0 *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+
+return (0);
+
+}
