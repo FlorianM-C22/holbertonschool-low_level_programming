69da556cd2f5ca23a94fbc6924078f7d4b3fb339
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-16 14:21:46 +0200

    Task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- This program will print the alphabet
+ * with the putchar function in lowercase then in UPPERCASE
+ * Return: 0 *
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 97 ; ch <= 122 ; ch++)
+
+	{
+	if (ch != 113 && ch != 102)
+	putchar(ch);
+	}
+}
