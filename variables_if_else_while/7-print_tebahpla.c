aee432fcc325db578b8b57f1498cce1e01a681cd
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-16 16:33:35 +0200

    Task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- This program will print the alphabet
+ * with the putchar function in lowercase then in UPPERCASE
+ * Return: 0 *
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 122 ; ch <= 97 ; ch++)
+		putchar(ch);
+
+return (0);
+}
