203b3960bda6f681c6f7acb83ead099458fafc80
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-16 18:29:35 +0200

    Task 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- This program will print all possible combinations of single-digit
+ * numbers, separated by a comma while using putchar
+ * Return: 0 *
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+		{
+		putchar('0' + i);
+		if (i != 9);
+		}
+
+			{
+				putchar(',');
+				putchar(' ');
+			}
+putchar('\n');
+return (0);
+}
