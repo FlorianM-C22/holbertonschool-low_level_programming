df2d18a15f1a75fa29f9fd84dd2936e013545ae4
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-16 13:58:40 +0200

    Task 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -4,16 +4,22 @@
 
 /**
  * main- This program will print the alphabet
- * with the putchar function
+ * with the putchar function in lowercase then in UPPERCASE
  * Return: 0 *
  */
 int main(void)
-
 {
 	char ch;
 
-	for (ch = 'a' ; ch <= 'z' ; ch++)
+	for (ch = 97 ; ch <= 122 ; ch++)
 		putchar(ch);
 	putchar('\n');
+
+	char ch_upper;
+
+	for (ch_upper = 65 ; ch_upper <= 90 ; ch_upper++)
+		putchar(ch_upper);
+	putchar('\n');
+
 	return (0);
 }
