c09dd14d10260f007c74ffcf6500cef65ca4acfb
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-17 15:26:27 +0200

    Task 3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * checks for lowercase character
+ * _islower - checks for lowercase character
  * Returns 1 if lowercase
  * Returns 0 if uppercase
  */
-int islower (int c)
+int _islower (int c)
 {
 	if (c >= 'a' && c <= 'z')
-	{
-		return 1;
-	}
+		{
+			return 1;
+		}
+
 	else
-	{
-		return 0;
-	}
+		{
+			return 0;
+		}
 }
