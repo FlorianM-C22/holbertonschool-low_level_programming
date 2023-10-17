8d46197cbe3461e3a0965e47c3642f0fc518dd03
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-17 12:30:05 +0200

    Project 2

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+main (void)
+{
+
+	{
+
+	char ch = '_putchar';
+	_putchar (ch);
+
+	}
+
+return (0);
+
+}
