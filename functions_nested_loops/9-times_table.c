555c930295c3e58152dde059b3f9154a42e4b457
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-18 10:58:37 +0200

    Task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,21 +6,18 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i = 0;
+	int j = 0;
 	int result = i * j;
 
 	for (i = 0; i <= 9; i++)
 		{
 			for (j = 0; j <= 9; j++);
-		}
 		_putchar ('0' + result);
 		_putchar (',');
 		_putchar (' ');
-		_putchar ('\n');
+		}
 
-	else
-	{
-		return;
-	}
+	_putchar ('\n');
 
 }
