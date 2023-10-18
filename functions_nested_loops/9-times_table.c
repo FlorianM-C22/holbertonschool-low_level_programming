7e92c3936a827dd9fcdbc7514853df861e3a365b
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-18 10:17:00 +0200

    Takws 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *times_table - prints the 9 times table, starting with 0
+ * Return: 0 success
+ */
+void times_table(void)
+{
+	int i, j;
+	int result = i * j;
+
+	for (i = 0; i <= 9; i++)
+		{
+			for (j = 0; j <= 9; j++);
+		}
+		_putchar ('0' + result);
+		_putchar (',');
+		_putchar (' ');
+		_putchar ('\n');
+
+	else
+	{
+		return;
+	}
+
+}
