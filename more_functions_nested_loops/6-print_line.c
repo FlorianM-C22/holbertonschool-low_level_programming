43cbbc1d2f35cd30f24580d23db1e1b3f6ea51cc
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-20 14:34:47 +0200

    Task 6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -7,16 +7,19 @@
  */
 void print_line(int n)
 {
-	{
-	if (n < 0)
+	while (n > 0)
+		{
+		_putchar('_');
+		n--;
+		}
+	_putchar('\n');
+
+	if (n <= 0)
 		{
 		_putchar('\n');
 		}
-
 	else
 		{
-		_putchar(n * '_');
 		_putchar('\n');
 		}
-	}
 }
