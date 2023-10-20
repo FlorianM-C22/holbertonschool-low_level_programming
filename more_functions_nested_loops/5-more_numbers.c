6c91e66295d78aab9907e1370daf146f99e9fdcc
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-20 11:59:45 +0200

    Task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,12 +12,13 @@ void more_numbers(void)
 	for (count = 0; count < 10; count++)
 	{
 		for (number = 0; number <= 14; number++)
+		{
 			if (number > 9)
 			{
 			_putchar('0' + (number / 10));
 			}
 			_putchar('0' + (number % 10));
-
+		}
 		_putchar('\n');
 	}
 }
