8fab74d8fede4aebc6e77716abff12e9899bb295
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-20 09:41:03 +0200

    Task 1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
 /**
- * isdigit - prints the alphabet
- * with a newline
+ *_isdigit - checks for a digit (0 through 9).
+ *@c: character checked
+ *Return: 0 Success
  */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
