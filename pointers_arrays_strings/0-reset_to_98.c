be2f2603fc23228725e3744f7bc29897c747d9fd
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-23 17:38:26 +0200

    Task 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - Write a function that takes a pointer to an int as parameter
+ *main -
+ *and updates the value it points to to 98.
+ *@n: value tested
+ *Return: Always 0.
+*/
+void reset_to_98(int *n)
+{
+	int n;
+	int *n = &reset_to_98;
+}
+
+int main()
+{
+	int n = 98;
+}
