e5d9a401a4043fd4b7aa723e30288ae5d641033a
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-24 14:41:20 +0200

    Task 7 - Added commentary

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,18 +8,20 @@
  *puts_half - function that prints half of a string
  *character, followed by a new line
  *@str : pointer tested
+ *@i: index value
+ *@n: strlen value
  *Return: Always 0.
 */
 void puts_half(char *str)
 {
-	int i, n, m;
+	int i, n, m; /*Declare initial values*/
 
 	i = 0;
 
-	n = strlen(str);
-	m = (n / 2);
+	n = strlen(str); /*String length*/
+	m = (n / 2); /*We need to divide the string by 2 to print half of it*/
 
-	while (str[i] != '\0')
+	while (str[i] != '\0') /*Initiate the loop*/
 	{
 		if (n <= m)
 			_putchar(str[i]);
