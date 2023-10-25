0abc014705cad832c39a9f786d94de7cfb296182
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-25 14:39:08 +0200

    Task 2.2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -3,27 +3,31 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stddef.h>
 
 /**
- *_strncpy - concatenates two strings
- *@dest: destination string
- *@src: source string
- *@n: numeric value to define the 'n' first octets of *src
- *main - check main.h
- *Return: Always 0.
-*/
+ * _strncpy - Copy a string up to 'n' characters
+ * @dest: Destination buffer
+ * @src: Source string
+ * @n: Maximum number of characters to copy
+ *
+ * Return: Pointer to the destination string 'dest'
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *dest_ptr = dest;
+	int i; /*Declare an integer variable to use as a loop counter*/
 
-	while (*src != '\0')
+	/* Loop through 'src' and 'dest' simultaneously */
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		*dest_ptr = *src;
-		dest_ptr++;
-		src++;
+		dest[i] = src[i];
 	}
 
-	dest[n] = '\0';
+	/* Pad 'dest' with null characters if necessary */
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
 
-	return (0);
+	return (dest);
 }
