7511978697ec0eebb08daeeaeeaab369e21b0095
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-25 22:28:04 +0200

    Task 6.6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -18,7 +18,7 @@ char *cap_string(char *str) /*Pointer to an array named str */
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i == 0 || isspace(str[i - 1]))
+		if (i == 0 || isspace(str[i - 1]) || ispunct(str[i - 1]))
 		{
 			str[i] = toupper(str[i]);
 		}
