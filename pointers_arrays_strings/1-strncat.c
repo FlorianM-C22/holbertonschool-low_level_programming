52d99524d0e5295d24775fbc7b9e52d896f0ec1e
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-25 12:18:26 +0200

    Task 1.1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -5,22 +5,24 @@
 #include <unistd.h>
 
 /**
- *_strcat - concatenates two strings
+ *_strncat - concatenates two strings
  *@dest: destination string
  *@src: source string
+ *@n: numeric value to define the 'n' first octets of *src
  *main - check main.h
  *Return: Always 0.
 */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	char *dest_start = dest; /*Temp char variable to copy src and dest*/
+	char *dest_start = dest; /*Char variable to return dest*/
 
 	while (*dest != '\0')
 	{
 		dest++;
 	}
-
 	strcpy(dest, src); /*Copy both strings and return them to a new buffer*/
+
+	dest[n] = '\0'; /*Adds a NULL character after to the chain + n*/
 
 	return (dest_start);
 }
