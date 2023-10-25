ee4e02649a19c3e3e35537f7e6e5416323927c8f
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-25 15:09:29 +0200

    Task 3.3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -14,8 +14,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
+	/*Tout pendant que s1 et s2 sont différents de null*/
 	while (*s1 != '\0' && *s2 != '\0')
 	{
+		/*Calcule et retourne la différence entre s1 et s2 */
 		if (*s1 - *s2)
 		{
 			return (*s1 - *s2);
