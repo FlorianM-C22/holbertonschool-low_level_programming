76ffef27131a16f707b9bd892f99b2d74c2f2f8a
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-26 20:49:18 +0200

    Task 7.7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,18 +8,28 @@
 
 /**
  * leet - we go full 1337
- * @char *: character string to be converted to uppercase
  * @s: string to test
  * Return: 0
  */
 char *leet(char *s) /*Pointer to an array named s*/
 {
 	size_t i;
+	size_t y;
 
-	char repl[] = "aAoOtTlL"
+	char repl[] = "aeotlAEOTL"; /*Letters to be replaced*/
+	char num[] = "4307143071"; /*Numbers that will be put instead*/
 
-	for (i = 0; s[i] != '\0'; i++)
+	/*For every char in str*/
+	for (i = 0; i < strlen(s); i++)
 	{
-
+		/*For every char in letters*/
+		for (y = 0; y < strlen(repl); y++)
+		{
+			/*Are current letters current numbers ?*/
+			if (repl[y] == s[i])
+				s[i] = num[y]; /*If yes, replace*/
+		}
 	}
+
+	return (s);
 }
