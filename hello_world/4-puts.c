22e1fafd9fc31ceedb7f6453a93481038388941d
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-26 16:15:35 +0200

    Corrected indentation on various tasks

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -7,8 +7,8 @@
 int main(void)
 {
 
-    char myStr[] = "\"Programming is like building a multilingual puzzle";
-    puts(myStr);
-    return (0);
+	char myStr[] = "\"Programming is like building a multilingual puzzle";
+	puts(myStr);
+	return (0);
 
 }
---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <ctype.h>
+
+/**
+ * leet - we go full 1337
+ * @char *: character string to be converted to uppercase
+ * @s: string to test
+ * Return: 0
+ */
+char *leet(char *s) /*Pointer to an array named s*/
+{
+	size_t i;
+
+	char repl[] = "aAoOtTlL"
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+}
