f7762b2d81383c85f741a197d1d2e769b587a7f8
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-29 18:41:04 +0100

    Task 7.3

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -18,8 +18,13 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-		_putchar(a[i][j]);
-		_putchar(' ');
+		{
+			_putchar(a[i][j]);
+			if (a[i][j] != ' ')
+			{
+				_putchar(' ');
+			}
+		}
 	}
 	_putchar('\n');
 }
