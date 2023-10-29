7ad6ac07fe916ecd30b14ce81b166d7bce7d77da
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-29 13:55:25 +0100

    Added comments to 1.3

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -17,17 +17,19 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
+	/*If s is NULL, return NULL to avoid any crashes*/
 	if (src == NULL)
 	{
 		return (NULL);
 	}
 
+	/*For every character of the src string which is not a NULL*/
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		{
-		dest[i] = src[i];
+		dest[i] = src[i]; /*Src and dest are equal (copying string)*/
 		}
 
-	if (i < n)
+	if (i < n) /*If i is smaller than n, dest[i] is NULL*/
 	{
 		dest[i] = '\0';
 	}
