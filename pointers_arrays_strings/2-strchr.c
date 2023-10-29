1c9a61a07fb1ddb27a304566b27b282f431e2218
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-29 14:15:01 +0100

    Task 2.3

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <ctype.h>
+
+/**
+ *_strchr - locates a character in a string
+ *@s: char string to be tested
+ *@c: char string to be tested
+ * Return: 0
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == c)
+	{
+		return (&s[i]);
+	}
+
+	return (NULL);
+}
