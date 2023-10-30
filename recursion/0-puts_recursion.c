d4d9d7ace5507ce32b214d16047117f8e8aabe71
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-30 13:08:04 +0100

    Task 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <ctype.h>
+
+/**
+ *_puts_recursion - prints a string followed by a new line
+ *@s: string to be printed
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	_putchar('\n');
+}
