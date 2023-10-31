c6951d4dbed0c58d1cdc438b158dfdd408e52fbe
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-31 10:58:12 +0100

    Task 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,9 +9,37 @@
 
 /**
  *is_prime_number - returns 1 if the input integer is a prime
- *is_divisible - checks if number is divisible
+ *prime_number_helper - helps us find prime numbers
  *@n: integer to be tested
- *@divisor: divisor value
+ *@guess: helps us guess if n is a prime number
  * Return: 0
  */
 
+int prime_number_helper(int n, int guess);
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+	return (prime_number_helper(n, n - 1));
+}
+/**
+ *prime_number_helper - helps us find prime numbers
+ *@n: integer to be tested
+ *@guess: helps us guess if n is a prime number
+ * Return: 0
+ */
+int prime_number_helper(int n, int guess)
+{
+	if (n <= 1)
+		return (0);
+
+	if (guess == 1)
+		return (1);
+
+	if (n % guess == 0)
+		return (0);
+
+	return (prime_number_helper(n, guess - 1));
+}
