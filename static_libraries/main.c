ebef1e87af83293846c596198aa8e05fb5dc9602
FlorianM-C22 <7484@holbertonstudents.com>
2023-10-31 16:37:14 +0100

    Task 1

--- static_libraries/main.c
+++ static_libraries/main.c
@@ -1,5 +1,8 @@
 #include "main.h"
-
+/**
+ * main - function for library project
+ * Return: 0
+*/
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
