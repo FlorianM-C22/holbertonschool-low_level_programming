df64c82040f353ecc37a138a4d5cfdb550b17ba4
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-01 17:58:44 +0100

    Task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 SUCCESS
+*/
+int main(int argc, char **argv)
+{
+	int n = strtol(argv[1], NULL, 10);
+	int m = strtoll(argv[2], NULL, 10);
+	int result = (n * m);
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", result);
+	}
+
+	return (0);
+}
