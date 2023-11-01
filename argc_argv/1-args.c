cd4becbe0940e6f47e9dce7ce1c2db1cd7459bc9
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-01 15:21:48 +0100

    Task 1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -11,12 +11,7 @@
 */
 int main(int argc, char **argv)
 {
-	int count;
-
-	for (count = 0; count < argc; count++)
-	{
-		printf("%s\n", argv[count]);
-	}
-
+	(void)argv;
+	printf("%d\n", argc);
 	return (0);
 }
