c0a3d6592a349c098bb48930dfa02a7ced4a3a3c
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-01 18:50:26 +0100

    Task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,19 +11,22 @@
 */
 int main(int argc, char **argv)
 {
-	int n = strtol(argv[1], NULL, 10);
-	int m = strtoll(argv[2], NULL, 10); /*Converts arguments as integers*/
-	int result = (n * m);
 
-	if (argc < 3)
+	int n = 0;
+	int m = 0;
+	int result = 0;
+
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		printf("%d\n", result); /*Prints the result*/
-	}
+
+	n = strtol(argv[1], NULL, 10);
+	m = strtol(argv[2], NULL, 10);
+	result = (n * m);
+
+	printf("%d\n", result); /*Prints the result*/
 
 	return (0);
 }
