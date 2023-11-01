0278832f892a5bdb10197bd6f1d38f919ccf05a7
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-01 19:08:22 +0100

    Task 1 - Code correction

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -6,12 +6,11 @@
 /**
  * main -  a program that prints its name, followed by a new line.
  * @argc: argument count
- * @argv: argument vector
+ * @argv: argument vector (unused)
  * Return: 0 SUCCESS
 */
-int main(int argc, char **argv)
+int main(int argc, __attribute__((unused)) char **argv)
 {
-	(void)argv;
 	printf("%d\n", argc - 1);
 	return (0);
 }
