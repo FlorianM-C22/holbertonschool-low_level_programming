366d64ef30c7289531b07af21a571ae76482bbd4
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-01 16:49:00 +0100

    Task 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main -  a program that prints all arguments it receives
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 SUCCESS
+*/
+int main(int argc, char **argv)
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+	{
+		printf("%s\n", argv[count]);
+	}
+
+	return (0);
+}
