78010158618ef2c8afcfa09375417e404ac0fb1d
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-06 20:15:55 +0100

    Added comments to task 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -14,9 +14,11 @@ int **alloc_grid(int width, int height)
 	int **arr1;
 	int i, j;
 
+	/*Exit condition if width or height = 0*/
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
+	/*Allocating memory of each line*/
 	arr1 = malloc(sizeof(int *) * height);
 
 	if (arr1 == NULL)
@@ -24,10 +26,12 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
+		/*Allocating memory for each column*/
 		arr1[i] = malloc(sizeof(int) * width);
 
 		if (arr1[i] == NULL)
 		{
+			/*Freeing memory if array is NULL*/
 			for (j = 0; j < i; j++)
 			{
 				free(arr1[j]);
@@ -38,9 +42,10 @@ int **alloc_grid(int width, int height)
 
 		for (j = 0; j < width; j++)
 		{
+			/*Printing the 2 dimensional array*/
 			arr1[i][j] = 0;
 		}
 	}
 
-	return (arr1);
+	return (arr1); /*Returning array*/
 }
