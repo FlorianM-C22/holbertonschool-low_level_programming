3e3f0cc9985862fba75ccafa906bef38377663af
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-06 17:08:24 +0100

    Task 1

---
+++ malloc_free/1-strdup(F).c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ * @str: copy of string
+ * Return: 0 SUCCESS
+*/
+char *_strdup(char *str)
+{
+	unsigned int i;
+	char *buffer;
+
+	if (str == NULL)
+		return (NULL);
+
+	buffer = malloc(strlen(str) * (sizeof(str)));
+
+	if (buffer == NULL)
+		return (NULL);
+
+	for (i = 0; i < strlen(str); i++)
+		buffer[i] = str[i];
+
+	return (buffer);
+
+	free(buffer);
+}
--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,31 +1,27 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
 /**
- * _strdup - returns a pointer to a newly allocated space in memory,
+ *_strdup - returns a pointer to a newly allocated space in memory,
  * which contains a copy of the string given as a parameter
  * @str: copy of string
- * Return: 0 SUCCESS
-*/
+ * Return: Always 0.
+ */
 char *_strdup(char *str)
 {
-	unsigned int i;
+	unsigned int i = 0;
 	char *buffer;
 
 	if (str == NULL)
-		return (NULL);
+		return NULL;
 
-	buffer = malloc(strlen(str) * (sizeof(str)));
+	buffer = malloc(sizeof(char) * strlen(str));
+	if (buffer == NULL) // bad alloc
+		return NULL;
 
-	if (buffer == NULL)
-		return (NULL);
-
-	for (i = 0; i < strlen(str); i++)
+	for (i = 0; i < strlen(str); ++i)
 		buffer[i] = str[i];
 
-	return (buffer);
-
-	free(buffer);
+	return buffer;
 }
