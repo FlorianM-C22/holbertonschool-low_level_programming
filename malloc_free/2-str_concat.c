61ec91880309beb1743a24bd538f5d95d327655e
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-06 20:28:10 +0100

    Task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,29 +11,27 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	size_t i, j; /*Declaring initial values*/
-	size_t size1 = strlen(s1);
-	size_t size2 = strlen(s2);
+	size_t i, j;
+	size_t size1 = (s1 != NULL) ? strlen(s1) : 0;
+	size_t size2 = (s2 != NULL) ? strlen(s2) : 0;
 	size_t totalSize = size1 + size2;
 
-	char *result = malloc(totalSize + 1); /*Allocating memory of s1 + s2*/
+	char *result = malloc(totalSize + 1);
 
 	if (result == NULL)
 		return (NULL);
 
-	if (s1 != NULL) /*Copying string's chars*/
-		for (i = 0; i < size1; i++)
-			result[i] = s1[i];
-	else
-		return (NULL);
+	for (i = 0; i < size1; i++)
+	{
+		result[i] = s1[i];
+	}
 
-	if (s2 != NULL)
-		for (j = 0; j < size2; j++)
-			result[i + j] = s2[j];
-	else
-		return (NULL);
+	for (j = 0; j < size2; j++)
+	{
+		result[i + j] = s2[j];
+	}
 
-	result[totalSize] = '\0'; /*Adding NULL character*/
+	result[totalSize] = '\0';
 
 	return (result);
 }
