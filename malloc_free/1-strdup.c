d74382d7f9bff6102228bee97e1919ae5b6c0b5b
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-06 17:11:48 +0100

    Task 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -17,7 +17,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	buffer = malloc(sizeof(char) * strlen(str));
+	buffer = malloc(sizeof(char) * (strlen(str) + 1));
 
 	if (buffer == NULL)
 		return (NULL);
