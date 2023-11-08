da91c53ade55a24d76fc50c467b76d5076fbce67
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-08 15:38:33 +0100

    Task 2

---
+++ preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "2-main.c"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf(FILENAME);
+	printf("\n");
+	return (0);
+}
---
+++ preprocessor/2-main.c
@@ -0,0 +1,6 @@
+#ifndef __protect__
+#define __protect__
+
+#define FILENAME "02-main.c"
+
+#endif
