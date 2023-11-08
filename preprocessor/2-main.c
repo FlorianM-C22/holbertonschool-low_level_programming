e7d3ae82da6ec035eda249d53925f19696cd9b5b
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-08 16:00:00 +0100

    Task 2

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include "02-main.c"
 
 /**
- *main - prints the name of source code file
+ * main - prints the name of the the file it was compiled from
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	printf("Name of source file: %s\n", __FILE__);
+	printf("%s\n", __FILE__);
+
 	return (0);
 }
