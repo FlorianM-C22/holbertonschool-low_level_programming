798ad68a87868286ceaa450fcdd1006c013272fb
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-13 17:40:26 +0100

    Task 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ *free_dog - doggy's free
+ *@d: pointer to structure
+ *Return: new dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		/* Free previously allocated memory */
+		free(d->name);
+		free(d->owner);
+
+		/* Free previously allocated memory */
+		free(d);
+	}
+}
