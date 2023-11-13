66345db1a13ed084f83c5e9d8a34d8a933e98a44
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-13 16:18:16 +0100

    Added comments to task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,20 +9,20 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d == NULL) /*If d is NULL return*/
 		return;
 
-	printf("Name: ");
-	if (d->name != NULL)
+	printf("Name: "); /*Print start of d->name*/
+	if (d->name != NULL) /*If not NULL print name*/
 		printf("%s\n", d->name);
 	else
-		printf("(nil)\n");
+		printf("(nil)\n"); /*else print (nil)*/
 
 	printf("Age: %f\n", d->age);
 
-	printf("Owner: ");
+	printf("Owner: "); /*Print start of d->owner*/
 	if (d->owner != NULL)
 		printf("%s\n", d->owner);
 	else
-		printf("(nil)\n");
+		printf("(nil)\n"); /*else print (nil)*/
 }
