781a057f9cd90c114919fc09d1271152d2fdb0f9
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-13 15:51:29 +0100

    Task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,26 +9,35 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d->age == '\0')
-	{
-		printf("(nil)");
-	}
-	if (d->owner == NULL)
-	{
-		printf("(nil)");
-	}
 	if (d->name == NULL)
 	{
 		printf("Name: (nil)");
 	}
-	if (d == NULL)
-	{
-		return;
-	}
 	else
 	{
 		printf("Name: %s\n", d->name);
+	}
+
+	if (d->age == '\0')
+	{
+		printf("(nil)");
+	}
+	else
+	{
 		printf("Age: %f\n", d->age);
+	}
+
+	if (d->owner == NULL)
+	{
+		printf("(nil)");
+	}
+	else
+	{
 		printf("Owner: %s\n", d->owner);
+	}
+
+	if (d == NULL)
+	{
+		return;
 	}
 }
