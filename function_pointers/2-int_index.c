b79027fe0764a888f44cc0a1da132d5fd96ce800
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-14 16:09:16 +0100

    Added documentation to task 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,15 +12,19 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i; /*Index value*/
 
+	/*Check for valid array and comparison function*/
 	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
+		/*Return -1 if array is invalid or size is less than or equal to 0*/
 
+	/*Iterate through the array elements*/
 	for (i = 0; i < size; i++)
 	{
+		/*Use the comparison function to check the current array element*/
 		if (cmp(array[i]) != 0)
-			return (i);
+			return (i); /*Return the index if the comparison is not 0*/
 	}
 
 	return (-1);
