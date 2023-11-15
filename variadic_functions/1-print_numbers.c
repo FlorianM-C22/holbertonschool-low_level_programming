d915aa36d4415fd6d1172a70d19463ef343cc811
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-15 13:56:55 +0100

    Task 1 - Added comments

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,18 +13,20 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int i;
+	unsigned int i;/*Initial variables*/
 
 	va_start(ap, n);
 
 	for (i = 1; i <= n; i++)
 	{
-		printf("%d", va_arg(ap, int));
+		printf("%d", va_arg(ap, int));/*Print integers*/
 
+		/*Checking if 1st number and char isn't NULL*/
 		if (i != n && separator != NULL)
 		printf("%s", separator);
 	}
 
-	va_end(ap);
+	va_end(ap);/*Cleaning the list*/
+
 	printf("\n");
 }
