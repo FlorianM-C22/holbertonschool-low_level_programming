24e5a3bfcec5f00506c5b51dfee0717cd3d4fafa
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-27 15:44:09 +0100

    Task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,15 +13,14 @@ size_t print_list(const list_t *h)
 	size_t n = 0;
 	const list_t *current = h;
 
-	if (current == NULL)
-	{
-		printf("[0] (nil)");
-		return (1);
-	}
-
 	while (current != NULL)
 	{
-		printf("%s\n", current->str);
+		if (current->str == NULL)
+			printf("[%u] (nil)\n", current->len);
+
+		else
+			printf("[%u] %s\n", current->len, current->str);
+
 		n++;
 		current = current->next;
 	}
