3146ec69be31f8862c0b43eb3107bdacbf39cd1a
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-29 16:03:38 +0100

    Task 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -9,21 +9,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result;
+	unsigned int k = 1;
 	unsigned int i = 0;
+	int c;
+	unsigned int len;
 
-	if (b == NULL)
-		return (0);
+	len = strlen(b);
 
-	while (b[i] == '0' || b[i] == '1')
+	for (c = len - 1; c >= 0; c--)
 	{
-		result <<= 1;
-		result += b[i] - '0';
-		i++;
+		if (b[c] != '0' && b[c] != '1')
+			return (0);
+		if (b[c] == '1')
+		{
+			i += k;
+		}
+		k *= 2;
 	}
+	return (i);
 
-	if (b[i] != '\0') /* Check for non-binary characters */
-		return (0);
-
-	return (result);
 }
