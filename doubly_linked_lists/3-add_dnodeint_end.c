f4ffc5f07b7b91e715e71ae0bc6ff2d7b8264d98
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-29 11:23:19 +0100

    Improved task 3

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,39 +1,45 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * add_dnodeint_end - adds a new node at the end of a dlistint_t list.
- *@head: pointer to the head of the linked list
- *@n: number of nodes to be added
- * Return: number of nodes in the linked list
+ * add_dnodeint_end - add a node at the end of a dlistint_s list
+ * @head: pointer to the list
+ * @n: where to insert the node
+ * Return: address of the last node
  */
+
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	dlistint_t *current = *head;
+	dlistint_t *last_node, *temp;
 
-	if (new_node == NULL)
+	last_node = malloc(sizeof(dlistint_t));
+
+	if (last_node == NULL)
 		return (NULL);
 
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
+	/*Adding data in the next node*/
+	last_node->n = n;
 
-	if (*head == NULL)
-		/* If the list is empty, the new node is now the first one */
-		*head = new_node;
+	/*Set pointers of the new node*/
+	last_node->next = NULL;
+
+	/*Checks if list is empty*/
+	if ((*head) == NULL)
+		(*head) = last_node;
+
 	else
 	{
-		/* Iterate through the last node of the list */
-		while (current->next != NULL)
-			current = current->next;
+		temp = *head;
 
-		 /* Adding the new node at the end of the list */
-		current->next = new_node;
-		new_node->prev = current;
+		/*Iterate through the list to find the last node*/
+		while (temp->next != NULL)
+			temp = temp->next;
+
+		/*Adds the node at the end of the list*/
+		temp->next = last_node;
+		last_node->prev = temp;
 	}
-
-	return (new_node);
+	return (last_node);
 }
