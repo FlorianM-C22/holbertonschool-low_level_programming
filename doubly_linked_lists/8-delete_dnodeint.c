d442e0ec510ef420825a95733e31a7d7ef3ec0a7
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-29 13:53:03 +0100

    Added comments to task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -15,31 +15,36 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *current, *temp;
 	unsigned int i;
 
+	/*Checks if list or head is NULL*/
 	if (head == NULL || *head == NULL)
 		return (-1);
 
+	/*Assign the head of the list to the current*/
 	current = *head;
 
+	/*If index = 0, delete 1st node*/
 	if (index == 0)
 	{
-		*head = current->next;
+		*head = current->next; /*Updating head to the next node*/
 		if (*head != NULL)
 			(*head)->prev = NULL;
 		free(current);
 		return (1);
 	}
-
+	/*Iterate to the node at specified index or at end of list*/
 	for (i = 0; i < index && current != NULL; i++)
 	{
 		current = current->next;
 	}
 
+	/*If specified index is beyond end of the list, ERROR*/
 	if (current == NULL)
 		return (-1);
 
 	temp = current->prev;
 	temp->next = current->next;
 
+	/*If deleted node is not the last one, update next's node pointer*/
 	if (current->next != NULL)
 		current->next->prev = temp;
 
