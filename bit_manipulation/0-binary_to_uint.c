2077074ccce7ab3a58f7f68f9b7f2eb1afc5eecb
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-30 13:04:16 +0100

    Task 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,10 +11,10 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int k = 1;
 	unsigned int i = 0;
-	unsigned int c;
+	int c;
 	unsigned int len;
 
-	unsigned int len = strlen(b);
+	len = strlen(b);
 
 	if (b == NULL)
 		return (0);
