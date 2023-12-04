9a3a692d6c8b4e21ee2770d500817fed453f5384
FlorianM-C22 <7484@holbertonstudents.com>
2023-12-04 17:57:27 +0100

    Task 2

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,50 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: Name of the file to be printed
+ * @text_content: File's content
+ * Return: 1 = SUCESS
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t file, wrt;
+
+	if (!filename)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_APPEND);
+
+	if (file == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		if (access(filename, F_OK) == 0)
+		{
+			close(file);
+			return (1);
+		}
+		else
+			return (-1);
+	}
+
+	wrt = write(file, filename, strlen(text_content));
+
+	if (wrt == -1)
+	{
+		close(file);
+		return (-1);
+	}
+
+	close(file);
+
+	return (1);
+}
