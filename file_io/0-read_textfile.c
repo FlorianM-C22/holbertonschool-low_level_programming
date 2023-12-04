838f116d23a3e554bad342ef307351cd81554a43
FlorianM-C22 <7484@holbertonstudents.com>
2023-12-04 12:33:51 +0100

    Task 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * filename - name of the file to be printed
+ * letters - Number of letters to be printed
+ * Return: The number of letters it could read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
