c30a2b87112979d3de756d5c0333acc0b64b2ddf
FlorianM-C22 <7484@holbertonstudents.com>
2023-12-04 18:19:34 +0100

    Added comments to task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -17,16 +17,21 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	ssize_t file, wrt;
 
+	/*If filename = NULL - ERROR*/
 	if (!filename)
 		return (-1);
 
+	/*Open file in write-only, appending to the end, permissions 664*/
 	file = open(filename, O_WRONLY | O_APPEND, 0664);
 
+	/*If file opening unsuccessful - ERROR*/
 	if (file == -1)
 		return (-1);
 
+	/*Checks if text_content is NULL*/
 	if (text_content == NULL)
 	{
+		/*Checks if the file exists*/
 		if (access(filename, F_OK) == 0)
 		{
 			close(file);
@@ -36,6 +41,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			return (-1);
 	}
 
+	/*Write the content to the file*/
 	wrt = write(file, text_content, strlen(text_content));
 
 	if (wrt == -1)
