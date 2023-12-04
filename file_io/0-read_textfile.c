640d6081de12dde097b4e27c9c9c2117a3b2e9e5
FlorianM-C22 <7484@holbertonstudents.com>
2023-12-04 15:05:20 +0100

    Task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -17,28 +17,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t n, fd;
 
 	if (filename == NULL)
-		return 0;
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
-	{
-		perror("open");
 		return (0);
-	}
 
 	n = read(fd, buf, letters);
 
 	if (n == -1)
 	{
-		perror("read");
 		close(fd);
 		return (0);
 	}
 
 	if (write(1, buf, n) != n)
 	{
-		perror("write");
 		close(fd);
 		return (0);
 	}
