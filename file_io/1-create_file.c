66b5a888da4fd76c96237b5a2c56728497c5395e
FlorianM-C22 <7484@holbertonstudents.com>
2023-12-04 17:05:25 +0100

    Task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -23,12 +23,14 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	file = creat(filename, 600);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file == -1)
 		return (-1);
 
 	write(file, text_content, strlen(text_content));
+
+	close(file);
 
 	return (1);
 }
