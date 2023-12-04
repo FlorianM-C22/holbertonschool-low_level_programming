249fa2216cfc24bdd55d3559418f4ae2842f09a0
FlorianM-C22 <7484@holbertonstudents.com>
2023-12-04 14:25:22 +0100

    Task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,14 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include "main.h"
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
- * filename - name of the file to be printed
- * letters - Number of letters to be printed
+ * @filename: Name of the file to be printed
+ * @letters: Number of letters to be printed
  * Return: The number of letters it could read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char buf[1024];
+	ssize_t n, fd;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	n = read(fd, buf, letters);
+
+	if (n == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
+	write(1, buf, n);
+
+	return (n);
 
 }
