a950ed6d0a85702c31b84d42ca300e9998482b84
FlorianM-C22 <7484@holbertonstudents.com>
2023-12-05 16:42:23 +0100

    Task 1

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,12 +9,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *newTable;
 	unsigned long int i;
-
+	/* Allocate memory for the hash table */
 	newTable = malloc(sizeof(hash_table_t));
 
 	if (newTable == NULL)
 		return (NULL);
-
+	/* Allocate memory for the array of hash nodes */
 	newTable->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (newTable->array == NULL)
@@ -22,10 +22,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(newTable);
 		return (NULL);
 	}
-
+	/* Initialize each array element to NULL */
 	for (i = 0; i < size; i++)
 		newTable->array[i] = NULL;
-
+	/* Set the size of the hash table */
 	newTable->size = size;
 
 	return (newTable);
