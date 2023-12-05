fc6c2e443cd35e97a62618cf5730abd4e034efff
FlorianM-C22 <7484@holbertonstudents.com>
2023-12-05 13:59:04 +0100

    Task 3 bien compressée bien illisible là

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,19 +4,16 @@
  * main - copies the content of a file to another file
  * @argc: Argument count
  * @argv: Argument value
- * Return: 0 = SUCCESS
+ * Return: 0 on success, 97-100 on error
  */
 int main(int argc, char *argv[])
 {
-	int source_fd, dest_fd, num;
-	int bytes;
+	int source_fd, dest_fd, num, bytes;
 	char buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
 	source_fd = open(argv[1], O_RDONLY);
 	if (source_fd == -1)
 	{
@@ -33,7 +30,7 @@ int main(int argc, char *argv[])
 	while ((bytes = read(source_fd, buffer, 1024)) > 0)
 	{
 		num = write(dest_fd, buffer, bytes);
-		if (num < bytes)
+		if (num != bytes)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(source_fd);
@@ -46,11 +43,9 @@ int main(int argc, char *argv[])
 		}
 	}
 	if (bytes == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	if (close(source_fd) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source_fd), exit(100);
-	if (close(dest_fd) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd), exit(100);
-
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+	if (close(source_fd) == -1 || close(dest_fd) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n"), exit(100);
 	return (0);
 }
