45cca2a52dd69ecc1925fb60c069f66293a10ac4
FlorianM-C22 <7484@holbertonstudents.com>
2023-12-05 18:07:27 +0100

    Task 3

--- hash_tables/3-main.c
+++ hash_tables/3-main.c
@@ -13,8 +13,6 @@ int main(void)
 	hash_table_t *ht;
 
 	ht = hash_table_create(1024);
-	hash_table_set(ht, "hetairas", "mentioner");
-	hash_table_set(ht, "heliotropes ", "neurospora");
-	hash_table_set(ht, "depravement", "serafins");
+	hash_table_set(ht, "betty", "cool");
 	return (EXIT_SUCCESS);
 }
