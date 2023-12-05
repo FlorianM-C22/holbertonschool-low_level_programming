dec7e45f3a02d723a6f6a2d29a0e8a403aa46598
FlorianM-C22 <7484@holbertonstudents.com>
2023-12-05 17:50:14 +0100

    Task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -32,23 +32,40 @@ hash_node_t *create_node(const char *key, const char *value)
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	char *dup_value;
 	unsigned long int index;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *temp;
 
 	/* Validate that the key is not an empty string */
-	if (key == NULL || key[0] == '\0')
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 
-	dup_value = strdup(value);
-
-	if (dup_value == NULL)
-		return (0);
-
-	/*Use the key_index function to calculate the index for the key*/
 	index = key_index((const unsigned char *)key, ht->size);
 
-	new_node = create_node(key, value);
+	/*Checks for collisions*/
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			if (temp->value == NULL)
+				return (0);
+			return (1);
+		}
+		temp = temp->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
 
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
--- hash_tables/3-main.c
+++ hash_tables/3-main.c
@@ -13,6 +13,8 @@ int main(void)
 	hash_table_t *ht;
 
 	ht = hash_table_create(1024);
-	hash_table_set(ht, "betty", "cool");
+	hash_table_set(ht, "hetairas", "mentioner");
+	hash_table_set(ht, "heliotropes ", "neurospora");
+	hash_table_set(ht, "depravement", "serafins");
 	return (EXIT_SUCCESS);
 }
