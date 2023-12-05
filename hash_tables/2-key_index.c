5dcd97453200e65ae6fa885729047e5d4ba201c2
FlorianM-C22 <7484@holbertonstudents.com>
2023-12-05 16:59:03 +0100

    Added comments to task 2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -9,9 +9,10 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+	/*Hash the Key using hash_djb2*/
 	unsigned long int hash_value = hash_djb2((const unsigned char *)key);
-
+	/*Map Hash Value to Index*/
 	unsigned long int index = hash_value % size;
-
+	/*Return Index*/
 	return (index);
 }
