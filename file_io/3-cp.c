f0c3d40dfdbe53210e95de038c4f193bc20e4af1
FlorianM-C22 <7484@holbertonstudents.com>
2023-12-05 14:02:56 +0100

    Task 3 DE MERDE

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,28 +6,29 @@
  * @argv: Argument value
  * Return: 0 on success, 97-100 on error
  */
+
 int main(int argc, char *argv[])
 {
-	int source_fd, dest_fd, num, bytes;
+	int source_fd, dest_fd, num;
+	int bytes;
 	char buffer[1024];
 
-	/* Check for correct number of arguments */
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	/* Open source file for reading */
 	source_fd = open(argv[1], O_RDONLY);
+
 	if (source_fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	/* Open or create destination file for writing */
 	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
 	if (dest_fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -35,18 +36,15 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	/* Read and write data in blocks of 1024 bytes */
 	while ((bytes = read(source_fd, buffer, 1024)) > 0)
 	{
 		num = write(dest_fd, buffer, bytes);
 
-		/* Check for successful write */
-		if (num != bytes)
+		if (num < bytes)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(source_fd);
 
-			/* Close destination file with error handling */
 			if (close(dest_fd) == -1)
 			{
 				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd);
@@ -56,17 +54,21 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	/* Check for read error */
 	if (bytes == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	/* Close files with error handling */
-	if (close(source_fd) == -1 || close(dest_fd) == -1)
+	if (close(source_fd) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source_fd);
+		exit(100);
+	}
+
+	if (close(dest_fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd);
 		exit(100);
 	}
 
