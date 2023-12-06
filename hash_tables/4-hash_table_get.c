856d7510ea579aa085717281234f27c8b1e7e4f4
FlorianM-C22 <7484@holbertonstudents.com>
2023-12-06 13:43:19 +0100

    Task 4

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: the hash table you want to add or update the key/value to
+ * @key: hash key, cannot be empty
+ * Return: value associated with the element,
+ * or NULL if key couldn’t be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	if (ht == NULL || key == NULL || ht->array == NULL || ht->size == 0)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+		current = current->next;
+	}
+	return (NULL);
+}
